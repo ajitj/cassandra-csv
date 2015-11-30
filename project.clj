518e4cbad7eba6ce87cd767d57c3b68d9babb83b
Ajit Joglekar <ajit.joglekar@gamesys.co.uk>
2015-11-30 16:26:41 +0000

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject cassandra-csv "0.3.0-SNAPSHOT"
+(defproject cassandra-csv "0.4.0-SNAPSHOT"
   :description "Cassandra export / import to / from csv"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cc.qbits/alia "2.8.0"]
+                 [cc.qbits/alia "2.12.0"]
                  [cc.qbits/hayt "3.0.0-rc2"]
                  [clj-time "0.11.0"]
                  [org.clojure/tools.cli "0.3.3"]
                  [org.clojure/data.csv "0.1.3"]
                  [intervox/clj-progress "0.2.1"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.5"]
+                 [org.slf4j/slf4j-log4j12 "1.7.13"]
                  [log4j "1.2.17"]]
   :main ^:skip-aot cassandra-csv.core
   :target-path "target/%s"
